2d9adaa1d6c58092d7dff093d396e7ebcf69e631
Zhixuan Yang <yangzhixuan18@gmail.com>
2022-06-08 18:50:14 +0000

    Change the text and add the counter

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -9,10 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+    private int pageViews = 0;
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    pageViews++;
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Zhixuan!</h1>");
+    response.getWriter().println("<p>This page has been viewed " + pageViews + " times.</p>");
   }
 }
