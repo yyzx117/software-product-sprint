91bc63385b7aa936bd2f7d0ac135a17625949d5e
Zhixuan Yang <yangzhixuan18@gmail.com>
2022-06-15 22:08:50 +0000

    week3 translation library

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -86,7 +86,24 @@ async function getServerStats() {
 
     const statsListElement = document.getElementById('server-stats-container');
     statsListElement.innerText = stat;
+}
 
+function requestTranslation() {
+    const text = document.getElementById('text1').value;
+    const languageCode = document.getElementById('language').value;
 
-    
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerText = 'Loading...';
+
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+
+    fetch('/translate', {
+        method: 'POST',
+        body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+    resultContainer.innerText = translatedMessage;
+    });
 }
